thestarentity/space-station-14

commit dfe79cc77e0eb59ba6769f7451e296f7d0ff306f

Predict BarSignBoundUserinterface (#42364)
fix bar sign prediction

Content.Client/BarSign/Ui/BarSignBoundUserInterface.cs
@@ -19,32 +19,27 @@ public sealed class BarSignBoundUserInterface(EntityUid owner, Enum uiKey) : Bou
         var sign = EntMan.GetComponentOrNull<BarSignComponent>(Owner)?.Current is { } current
             ? _prototype.Index(current)
             : null;
-        var allSigns = Shared.BarSign.BarSignSystem.GetAllBarSigns(_prototype)
+        var allSigns = BarSignSystem.GetAllBarSigns(_prototype)
             .OrderBy(p => Loc.GetString(p.Name))
             .ToList();
         _menu = new(sign, allSigns);
 
         _menu.OnSignSelected += id =>
         {
-            SendMessage(new SetBarSignMessage(id));
+            SendPredictedMessage(new SetBarSignMessage(id));
         };
 
         _menu.OnClose += Close;
         _menu.OpenCentered();
     }
 
-    public void Update(ProtoId<BarSignPrototype>? sign)
+    public override void Update()
     {
-        if (_prototype.Resolve(sign, out var signPrototype))
-            _menu?.UpdateState(signPrototype);
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        base.Dispose(disposing);
-        if (!disposing)
+        if (!EntMan.TryGetComponent<BarSignComponent>(Owner, out var signComp))
             return;
-        _menu?.Dispose();
+
+        if (_prototype.Resolve(signComp.Current, out var signPrototype))
+            _menu?.UpdateState(signPrototype);
     }
 }
 

Content.Shared/BarSign/BarSignSystem.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 
@@ -9,10 +10,14 @@ public sealed class BarSignSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+
 
     public override void Initialize()
     {
         SubscribeLocalEvent<BarSignComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<BarSignComponent, AfterAutoHandleStateEvent>(OnAfterAutoHandleState);
         Subs.BuiEvents<BarSignComponent>(BarSignUiKey.Key,
             subs =>
         {
@@ -29,25 +34,49 @@ public sealed class BarSignSystem : EntitySystem
         SetBarSign(ent, newPrototype);
     }
 
+    private void OnAfterAutoHandleState(Entity<BarSignComponent> ent, ref AfterAutoHandleStateEvent args)
+    {
+        // Update the UI if the component was changed.
+        if (_ui.TryGetOpenUi(ent.Owner, BarSignUiKey.Key, out var bui))
+            bui.Update();
+    }
+
     private void OnSetBarSignMessage(Entity<BarSignComponent> ent, ref SetBarSignMessage args)
     {
         if (!_prototypeManager.Resolve(args.Sign, out var signPrototype))
             return;
 
+        if (signPrototype.Hidden)
+            return; // Hidden signs cannot be selected from the BUI.
+
         SetBarSign(ent, signPrototype);
     }
 
+    /// <summary>
+    /// Set the sprite, name and description of the bar sign to a given <see cref="BarSignPrototype"/>.
+    /// </summary>
     public void SetBarSign(Entity<BarSignComponent> ent, BarSignPrototype newPrototype)
     {
+        if (ent.Comp.Current == newPrototype.ID)
+            return;
+
         var meta = MetaData(ent);
         var name = Loc.GetString(newPrototype.Name);
         _metaData.SetEntityName(ent, name, meta);
         _metaData.SetEntityDescription(ent, Loc.GetString(newPrototype.Description), meta);
+        _appearance.SetData(ent.Owner, BarSignVisuals.BarSignPrototype, newPrototype.ID);
 
         ent.Comp.Current = newPrototype.ID;
         Dirty(ent);
+
+        // Predict updating the BUI if it's open.
+        if (_ui.TryGetOpenUi(ent.Owner, BarSignUiKey.Key, out var bui))
+            bui.Update();
     }
 
+    /// <summary>
+    /// Returns a list of all <see cref="BarSignPrototype"/>s that are not hidden.
+    /// </summary>
     public static List<BarSignPrototype> GetAllBarSigns(IPrototypeManager prototypeManager)
     {
         return prototypeManager

Content.Client/BarSign/BarSignSystem.cs
@@ -1,53 +1,0 @@
-using Content.Client.BarSign.Ui;
-using Content.Shared.BarSign;
-using Content.Shared.Power;
-using Robust.Client.GameObjects;
-using Robust.Shared.Prototypes;
-
-namespace Content.Client.BarSign;
-
-public sealed class BarSignSystem : VisualizerSystem<BarSignComponent>
-{
-    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly UserInterfaceSystem _ui = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeLocalEvent<BarSignComponent, AfterAutoHandleStateEvent>(OnAfterAutoHandleState);
-    }
-
-    private void OnAfterAutoHandleState(EntityUid uid, BarSignComponent component, ref AfterAutoHandleStateEvent args)
-    {
-        if (_ui.TryGetOpenUi<BarSignBoundUserInterface>(uid, BarSignUiKey.Key, out var bui))
-            bui.Update(component.Current);
-
-        UpdateAppearance(uid, component);
-    }
-
-    protected override void OnAppearanceChange(EntityUid uid, BarSignComponent component, ref AppearanceChangeEvent args)
-    {
-        UpdateAppearance(uid, component, args.Component, args.Sprite);
-    }
-
-    private void UpdateAppearance(EntityUid id, BarSignComponent sign, AppearanceComponent? appearance = null, SpriteComponent? sprite = null)
-    {
-        if (!Resolve(id, ref appearance, ref sprite))
-            return;
-
-        AppearanceSystem.TryGetData<bool>(id, PowerDeviceVisuals.Powered, out var powered, appearance);
-
-        if (powered
-            && sign.Current != null
-            && _prototypeManager.Resolve(sign.Current, out var proto))
-        {
-            SpriteSystem.LayerSetSprite((id, sprite), 0, proto.Icon);
-            sprite.LayerSetShader(0, "unshaded");
-        }
-        else
-        {
-            SpriteSystem.LayerSetRsiState((id, sprite), 0, "empty");
-            sprite.LayerSetShader(0, null, null);
-        }
-    }
-}

Content.Client/BarSign/BarSignVisualizerSystem.cs
@@ -1,0 +1,30 @@
+using Content.Shared.BarSign;
+using Content.Shared.Power;
+using Robust.Client.GameObjects;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client.BarSign;
+
+public sealed class BarSignVisualizerSystem : VisualizerSystem<BarSignComponent>
+{
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+
+    protected override void OnAppearanceChange(EntityUid uid, BarSignComponent component, ref AppearanceChangeEvent args)
+    {
+        AppearanceSystem.TryGetData<bool>(uid, PowerDeviceVisuals.Powered, out var powered, args.Component);
+        AppearanceSystem.TryGetData<string>(uid, BarSignVisuals.BarSignPrototype, out var currentSign, args.Component);
+
+        if (powered
+            && currentSign != null
+            && _prototypeManager.Resolve<BarSignPrototype>(currentSign, out var proto))
+        {
+            SpriteSystem.LayerSetSprite((uid, args.Sprite), 0, proto.Icon);
+            args.Sprite?.LayerSetShader(0, "unshaded");
+        }
+        else
+        {
+            SpriteSystem.LayerSetRsiState((uid, args.Sprite), 0, "empty");
+            args.Sprite?.LayerSetShader(0, null, null);
+        }
+    }
+}

Content.Shared/BarSign/BarSignComponent.cs
@@ -4,6 +4,10 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.BarSign;
 
+/// <summary>
+/// Makes it possible to switch this entity's sprite and name using a BUI.
+/// <seealso cref="BarSignPrototype"/>
+/// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class BarSignComponent : Component
 {
@@ -14,14 +18,32 @@ public sealed partial class BarSignComponent : Component
     public ProtoId<BarSignPrototype>? Current;
 }
 
+/// <summary>
+/// The key for the BoundUserInterface.
+/// </summary>
 [Serializable, NetSerializable]
 public enum BarSignUiKey : byte
 {
     Key
 }
 
+/// <summary>
+/// The enum to be used for appearance data of the bar sign.
+/// </summary>
+[Serializable, NetSerializable]
+public enum BarSignVisuals : byte
+{
+    BarSignPrototype,
+}
+
+/// <summary>
+/// Send from the client when setting the bar sign.
+/// </summary>
 [Serializable, NetSerializable]
 public sealed class SetBarSignMessage(ProtoId<BarSignPrototype> sign) : BoundUserInterfaceMessage
 {
+    /// <summary>
+    /// The new prototype to use.
+    /// </summary>
     public ProtoId<BarSignPrototype> Sign = sign;
 }

commit 61e89cb77adb33ef28397016be88a9537dca5a2d

Fixed redundant bar sign windows (#42960)
Fixed redundant bar signs

* Fixes the extra bar sign popups bug introduced in #42364
by making BarSignBUI use CreateWindow

* Fixes bar signs placed from Entity Spawn Window

Content.Client/BarSign/Ui/BarSignBoundUserInterface.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Content.Shared.BarSign;
 using JetBrains.Annotations;
+using Robust.Client.UserInterface;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client.BarSign.Ui;
@@ -16,13 +17,12 @@ public sealed class BarSignBoundUserInterface(EntityUid owner, Enum uiKey) : Bou
     {
         base.Open();
 
-        var sign = EntMan.GetComponentOrNull<BarSignComponent>(Owner)?.Current is { } current
-            ? _prototype.Index(current)
-            : null;
         var allSigns = BarSignSystem.GetAllBarSigns(_prototype)
             .OrderBy(p => Loc.GetString(p.Name))
             .ToList();
-        _menu = new(sign, allSigns);
+
+        _menu = this.CreateWindow<BarSignMenu>();
+        _menu.LoadSigns(allSigns);
 
         _menu.OnSignSelected += id =>
         {
@@ -30,16 +30,17 @@ public sealed class BarSignBoundUserInterface(EntityUid owner, Enum uiKey) : Bou
         };
 
         _menu.OnClose += Close;
-        _menu.OpenCentered();
+        _menu.OpenToLeft();
     }
 
     public override void Update()
     {
-        if (!EntMan.TryGetComponent<BarSignComponent>(Owner, out var signComp))
+        if (!EntMan.TryGetComponent<BarSignComponent>(Owner, out var signComp)
+            || !_prototype.Resolve(signComp.Current, out var signPrototype))
             return;
 
-        if (_prototype.Resolve(signComp.Current, out var signPrototype))
-            _menu?.UpdateState(signPrototype);
+        _menu?.UpdateState(signPrototype);
     }
+
 }
 

Content.Client/BarSign/Ui/BarSignMenu.xaml.cs
@@ -8,23 +8,13 @@ namespace Content.Client.BarSign.Ui;
 [GenerateTypedNameReferences]
 public sealed partial class BarSignMenu : FancyWindow
 {
-    private string? _currentId;
-
-    private readonly List<BarSignPrototype> _cachedPrototypes = new();
+    private List<BarSignPrototype> _cachedPrototypes = new();
 
     public event Action<string>? OnSignSelected;
 
-    public BarSignMenu(BarSignPrototype? currentSign, List<BarSignPrototype> signs)
+    public BarSignMenu()
     {
         RobustXamlLoader.Load(this);
-        _currentId = currentSign?.ID;
-
-        _cachedPrototypes.Clear();
-        _cachedPrototypes = signs;
-        foreach (var proto in _cachedPrototypes)
-        {
-            SignOptions.AddItem(Loc.GetString(proto.Name));
-        }
 
         SignOptions.OnItemSelected += idx =>
         {
@@ -32,18 +22,21 @@ public sealed partial class BarSignMenu : FancyWindow
             SignOptions.SelectId(idx.Id);
         };
 
-        if (currentSign != null)
+    }
+
+    public void LoadSigns(List<BarSignPrototype> signs)
+    {
+        _cachedPrototypes.Clear();
+        _cachedPrototypes = signs;
+
+        foreach (var proto in _cachedPrototypes)
         {
-            var idx = _cachedPrototypes.IndexOf(currentSign);
-            SignOptions.TrySelectId(idx);
+            SignOptions.AddItem(Loc.GetString(proto.Name));
         }
     }
 
     public void UpdateState(BarSignPrototype newSign)
     {
-        if (_currentId != null && newSign.ID == _currentId)
-            return;
-        _currentId = newSign.ID;
         var idx = _cachedPrototypes.IndexOf(newSign);
         SignOptions.TrySelectId(idx);
     }

Content.Shared/BarSign/BarSignSystem.cs
@@ -31,10 +31,12 @@ public sealed class BarSignSystem : EntitySystem
 
     private void OnMapInit(Entity<BarSignComponent> ent, ref MapInitEvent args)
     {
-        if (ent.Comp.Current != null)
+        BarSignPrototype? newPrototype;
+        if (ent.Comp.Current is null)
+            newPrototype = _random.Pick(GetAllBarSigns(_prototypeManager));
+        else if (!_prototypeManager.Resolve(ent.Comp.Current, out newPrototype))
             return;
 
-        var newPrototype = _random.Pick(GetAllBarSigns(_prototypeManager));
         SetBarSign(ent, newPrototype);
     }
 
@@ -77,9 +79,6 @@ public sealed class BarSignSystem : EntitySystem
     /// </summary>
     public void SetBarSign(Entity<BarSignComponent> ent, BarSignPrototype newPrototype)
     {
-        if (ent.Comp.Current == newPrototype.ID)
-            return;
-
         if (HasComp<EmpDisabledComponent>(ent))
             return;
 