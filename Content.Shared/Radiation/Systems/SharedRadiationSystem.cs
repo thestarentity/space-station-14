a7b38e4cdbe5ab3f142ccbe0803ff3c230602ac7
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-04-06 22:14:37 -0700

    fix a couple radiation system bugs as a treat

---
+++ Content.Shared/Radiation/Systems/SharedRadiationSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Radiation.Components;
+
+namespace Content.Shared.Radiation.Systems;
+
+public abstract partial class SharedRadiationSystem : EntitySystem
+{
+    [Dependency] protected readonly EntityQuery<RadiationSourceComponent> SourceQuery = default!;
+
+    public void SetIntensity(Entity<RadiationSourceComponent?> entity, float intensity)
+    {
+        if (!SourceQuery.Resolve(entity, ref entity.Comp))
+            return;
+
+        entity.Comp.Intensity = intensity;
+        UpdateSource((entity, entity.Comp));
+    }
+
+    protected virtual void UpdateSource(Entity<RadiationSourceComponent> entity)
+    {
+
+    }
+}
