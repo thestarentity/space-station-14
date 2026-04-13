807e9137d1db90a49423d1c47fb210df2b1ea8da
Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
2026-04-13 15:36:41 -0700

    asfafasasffas

--- Content.Shared/Radiation/Systems/SharedRadiationSystem.cs
+++ Content.Shared/Radiation/Systems/SharedRadiationSystem.cs
@@ -6,6 +6,11 @@ public abstract partial class SharedRadiationSystem : EntitySystem
 {
     [Dependency] protected readonly EntityQuery<RadiationSourceComponent> SourceQuery = default!;
 
+    /// <summary>
+    /// Sets the intensity of a <see cref="RadiationSourceComponent"/> to the passed intensity.
+    /// </summary>
+    /// <param name="entity">Radiation source we're attempting to update</param>
+    /// <param name="intensity">Intensity we're setting the source to.</param>
     public void SetIntensity(Entity<RadiationSourceComponent?> entity, float intensity)
     {
         if (!SourceQuery.Resolve(entity, ref entity.Comp, false))
@@ -15,8 +20,8 @@ public abstract partial class SharedRadiationSystem : EntitySystem
         UpdateSource((entity, entity.Comp));
     }
 
-    protected virtual void UpdateSource(Entity<RadiationSourceComponent> entity)
-    {
-
-    }
+    /// <summary>
+    /// Updates the radiation source cache. Does nothing on client, see server!
+    /// </summary>
+    protected virtual void UpdateSource(Entity<RadiationSourceComponent> entity) { }
 }
