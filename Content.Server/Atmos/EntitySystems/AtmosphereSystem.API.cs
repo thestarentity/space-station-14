904844848d834bbc6f78420c092da5609b817dfa
ArtisticRoomba <145879011+ArtisticRoomba@users.noreply.github.com>
2026-04-18 22:35:33 -0700

    Cleanup passive valve (#43552)
    * file scope
    
    * cleanup and method extraction
    
    * inline and rename var
    
    * fix ema
    
    * (in HL scientist voice) this should help
    
    * cleanup comp as well while we're here

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.API.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.API.cs
@@ -806,6 +806,26 @@ public partial class AtmosphereSystem
         return contains;
     }
 
+    /// <summary>
+    /// Applies an exponential moving average to a value, given a new value, an old value, and a time delta.
+    /// </summary>
+    /// <param name="newValue">The new value to factor into the average.</param>
+    /// <param name="oldValue">The old value to factor into the average.</param>
+    /// <param name="deltaTime">The time delta to factor into the average.</param>
+    /// <param name="tau">The time constant to use for the average.
+    /// Higher values will make the average change more slowly,
+    /// while lower values will make it change more quickly.</param>
+    /// <returns>The result of the exponential moving average.</returns>
+    [PublicAPI]
+    public static float ExponentialMovingAverage(float newValue,
+        float oldValue,
+        float deltaTime,
+        float tau = 0.5f)
+    {
+        var a = deltaTime / tau;
+        return a * newValue + (1 - a) * oldValue;
+    }
+
     [ByRefEvent]
     private record struct SetSimulatedGridMethodEvent(
         EntityUid Grid,
